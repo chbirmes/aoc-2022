46e43695b0a103d2e010d85b9b4b45f87b20ffb1
Christian Birmes <christian.birmes@cologne-intelligence.de>
2022-12-01 10:39:33 +0100

    Solution Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun descendingGroupSums(input: String) =
+        input.split("\n\n")
+            .map { group -> group.split("\n").sumOf { it.toInt() } }
+            .sortedDescending()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: String) =
+        descendingGroupSums(input)
+            .first()
 
-    val input = readInput("Day01")
+    fun part2(input: String) =
+        descendingGroupSums(input)
+            .take(3)
+            .sum()
+
+    val testInput = readInputAsString("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
+    val input = readInputAsString("Day01")
     println(part1(input))
     println(part2(input))
 }
