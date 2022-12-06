a424019d3d178b302ab240adcd11c8621f7dcfb8
Christian Birmes <christian.birmes@cologne-intelligence.de>
2022-12-06 14:05:06 +0100

    Solution Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    fun part1(input: String): Int = input.signalStartIndex(4)
+
+    fun part2(input: String): Int = input.signalStartIndex(14)
+
+    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part1("nppdvjthqldpwncqszvftbrmjlhg") == 6)
+    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
+    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
+
+    check(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    check(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
+
+    val input = readInputAsString("Day06")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun String.signalStartIndex(markerLength: Int) =
+    windowedSequence(markerLength)
+        .indexOfFirst { it.toSet().size == markerLength } + markerLength
